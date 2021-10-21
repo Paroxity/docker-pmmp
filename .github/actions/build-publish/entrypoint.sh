59c3fbcff6568d003087d862c41dd7d1846d2304
aericio <16523741+Aericio@users.noreply.github.com>
2021-10-20 16:56:16 -1000

    Switch to paroxity/pmmp images

--- .github/actions/build-publish/entrypoint.sh
+++ .github/actions/build-publish/entrypoint.sh
@@ -23,11 +23,15 @@ export DOCKER_BUILDKIT=1
 
 # check if we should pull existing images to help speed up the build
 if [ "${INPUT_PULL}" == "true" ]; then
+  sh -c "docker pull paroxity/pmmp:'$TAG'"
 	sh -c "docker pull paroxity/pmmp-phpstan:'$TAG'"
 fi
+
+# build the base pmmp image
+sh -c "cd pocketmine-mp && docker build --cache-from paroxity/pmmp:'$TAG' -t paroxity/pmmp:'$TAG' --build-arg PMMP_TAG='$TAG' --build-arg BUILDKIT_INLINE_CACHE=1 ."
 
 # build the pmmp phpstan image
 sh -c "docker build --cache-from paroxity/pmmp-phpstan:'$TAG' -t paroxity/pmmp-phpstan:'$TAG' --build-arg TAG='$TAG' --build-arg BUILDKIT_INLINE_CACHE=1 ."
 
 # publish the builds to docker hub
-sh -c "docker push paroxity/pmmp-phpstan:'$TAG'"
+sh -c "docker push paroxity/pmmp:'$TAG' && docker push paroxity/pmmp-phpstan:'$TAG'"
